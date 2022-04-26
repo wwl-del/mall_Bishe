c7fdb8fe13e954f941065fbc78623d051fbdd1f4
Wwl <1832289379@qq.com>
2022-04-26 13:32:31 +0800

    项目文件

---
+++ mall-user-mange/src/main/java/com/nefu/mall/user/MallUserMangeApplication.java
@@ -0,0 +1,13 @@
+package com.nefu.mall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUserMangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallUserMangeApplication.class, args);
+	}
+
+}
